dcdc1727015d6dd068933041b0f1ad1cf3a93e5f
ahartfelder <ahartfelder@gmail.com>
2024-03-14 10:14:30 -0300

    config for ejs + favicon

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# login-form
-This is an example of login form using HTML and CSS.
+# Login Form Example with ExpressJS
+
+This is a sample login form built using HTML, CSS, and JavaScript with ExpressJS.
+
+## Installation
+
+To install the required dependencies, run:
+
+```bash
+npm install
+```
+
+## Running the Server
+
+To start the server, run:
+
+```bash
+npm start
+```
+
+## Accessing the Login Page
+
+After starting the server, navigate to the following URL in your browser:
+
+[Login Page](http://localhost:3000/login)
+
+You can now explore the login form. Feel free to try out various validations, use the reveal password button, and resize the page to test responsiveness.
--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src/views"));
+app.set("layout", "layouts/layout");
+
+app.use(expressLayouts);
+app.use(express.static(path.resolve("public")));
+
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/login", (req, res) => {
+  res.render("pages/login", { title: "Login Page" });
+});
 
 app.listen(PORT, () => console.log("Running server on port %s", PORT));
